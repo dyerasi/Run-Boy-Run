2c81db6f8639a0f44ae7a7087e82392910bb45a6
Akanksha <akankshadhanwal@akanksha.home>
2020-05-23 17:18:29 -0700

    improved code readability and added texture file for path

--- main-scene.js
+++ main-scene.js
@@ -26,79 +26,51 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
 class Assignment_Three_Scene extends Scene_Component
   { constructor( context, control_box )     // The scene begins by requesting the camera, shapes, and materials it will need.
       { super(   context, control_box );    // First, include a secondary Scene that provides movement controls:
-        if( !context.globals.has_controls   ) 
-          context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) ); 
-//35.05, 18.71, 181.73
-//37.5, 50,200
+        if( !context.globals.has_controls   )
+            context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) );
         context.globals.graphics_state.camera_transform = Mat4.look_at( Vec.of( 36.59, 20.14, -40.49 ), Vec.of( 1,0,-3 ), Vec.of( 0,1,0 ) );
         this.initial_camera_location = Mat4.inverse( context.globals.graphics_state.camera_transform );
 
         const r = context.width/context.height;
         context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
         
-        //movement booleans
-        var jump_bool = 0; 
-        var left_bool = 0;
-        var right_bool = 0;
-        var jump_end = 0;
-        var right_end = 0;
-        var left_end = 0;
+        // ***movement booleans***
+        this.jump_bool = 0;
+        this.left_bool = 0;
+        this.right_bool = 0;
+        this.jump_end = 0;
+        this.right_end = 0;
+        this.left_end = 0;
 
 
-        const shapes = { 
-                        box: new Cube(),
-                        torus:  new Torus( 15, 15 ),
-                         torus2: new ( Torus.prototype.make_flat_shaded_version() )( 15, 15 ),
- 
-                                // TODO:  Fill in as many additional shape instances as needed in this key/value table.
-                                //        (Requirement 1)
-                         sphere1: new (Subdivision_Sphere.prototype.make_flat_shaded_version())(1),
+        const shapes = {
+                         box: new Cube(),
                          sphere2: new (Subdivision_Sphere.prototype.make_flat_shaded_version())(2),
                          sphere3: new (Subdivision_Sphere)(3),
                          sphere4: new (Subdivision_Sphere)(4)
                        }
+
         this.submit_shapes( context, shapes );
                                      
                                      // Make some Material objects available to you:
         this.materials =
           { test:     context.get_instance( Phong_Shader ).material( Color.of( 1,1,0,1 ), { ambient:.2 } ),
-            ring:     context.get_instance( Ring_Shader  ).material(),
 
-                                // TODO:  Fill in as many additional material objects as needed in this key/value table.
-                                //        (Requirement 1)
-                                
-            sun: context.get_instance(Phong_Shader).material( 
+            path: context.get_instance(Phong_Shader).material(
               Color.of(1,0,1,1),
               {ambient:1}
             ),
-            planet_1: context.get_instance(Phong_Shader).material(
-              Color.of(221/255, 238/255, 245/255, 1), //Ice-grey/Azureish White
-              {ambient: 0}, 
-              {diffusivity: 1},
-              {specularity: 0}
-            ),
-            planet_2: context.get_instance(Phong_Shader).material(
-              Color.of(65/255, 104/255, 37/255, 1), //Swampy green-blue
-              {ambient:  0},  //CHANGE THIS AFTER TESTING!!!!!
-              {diffusivity: 0.2},
-              {specularity: 1}
-            ),
-           planet_3: context.get_instance(Phong_Shader).material(
-              Color.of(0.7843, 0.3922, 0.0431, 1), //muddy brown orange
-              {ambient:  0},  //CHANGE THIS AFTER TESTING!!!!!
-              {diffusivity: 1},
-              {specularity: 1}
-            ),
-           planet_4: context.get_instance(Phong_Shader).material(
+
+            player: context.get_instance(Phong_Shader).material(
               Color.of(0.678, 0.847, 0.902, 1), //soft light blue
               {specularity: 1},
-              {ambient: 0}
+              {ambient: 1}
             ),
-
           }
 
         this.lights = [ new Light( Vec.of( 5,-10,5,1 ), Color.of( 0, 1, 1, 1 ), 1000 ) ];
       }
+
     make_control_panel()            // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
       { //this.key_triggered_button( "Intial",  [ "0" ], () => this.attached = () => this.initial_camera_location );
         this.new_line();
@@ -106,43 +78,27 @@ class Assignment_Three_Scene extends Scene_Component
         this.key_triggered_button( "Left",     [ "a" ], () => this.left_bool = 0, undefined, () => this.left_bool = 1 );
         this.key_triggered_button( "Right",     [ "s" ], () => this.right_bool = 0, undefined, () => this.right_bool = 1 );
       }
+
     draw_path(box_size, row_length, path_length, model_transform, tr_right, graphics_state){
       let grey = Color.of(169/255,169/255,169/255, 1);
-      let yellow = Color.of(1,1,0,1);
-      const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
+      const t = graphics_state.animation_time / 1000;
        
        //draw path
-       
-
-        for(var i = 1; i != path_length+1; i++){
-          for(var j = 1; j != row_length + 1; j++){
-
-            this.shapes.box.draw( graphics_state, model_transform, this.materials.sun.override({color: grey}));
-           
-
+        for(let i = 1; i !== path_length+1; i++){
+          for(let j = 1; j !== row_length + 1; j++){
+            this.shapes.box.draw( graphics_state, model_transform, this.materials.path.override({color: grey}));
             //create boundary on edges
-            if((j ==1 || j == row_length))
+            if((j ===1 || j === row_length))
             {
-             
-
               model_transform = model_transform.times(Mat4.translation([0, tr_right, 0]));
-
-              this.shapes.box.draw( graphics_state, model_transform, this.materials.sun.override({color: Color.of(.5 + .5 * Math.sin(i * Math.PI * t), 0, .5 -.5 * Math.sin(j * Math.PI * t), 1)}));
-
+              this.shapes.box.draw( graphics_state, model_transform, this.materials.path.override({color: Color.of(.5 + .5 * Math.sin(i * Math.PI * t), 0, .5 -.5 * Math.sin(j * Math.PI * t), 1)}));
               model_transform = model_transform.times(Mat4.translation([0, -tr_right, 0]));
-
-
             }
-
-           
             model_transform = model_transform.times(Mat4.translation([ tr_right,0, 0]));
-            
 
           } //for loop for j
            model_transform = model_transform.times(Mat4.translation([tr_right * -1 * row_length, 0, 0]));
-
-            
-           if(i % 110 == 0 || i % 20 == 0 || i % 45 == 0) //create a pit at cycles of 20, 45, and 100
+           if(i % 110 === 0 || i % 20 === 0 || i % 45 === 0) //create a pit at cycles of 20, 45, and 100
            {
              for (let k = 0; k < 3; k++)
                model_transform = model_transform.times(Mat4.translation([0, 0, 2]));
@@ -151,7 +107,6 @@ class Assignment_Three_Scene extends Scene_Component
            {
              model_transform = model_transform.times(Mat4.translation([0, 0, 2]));
            }
-
 
         } //for loop for i
     }
@@ -164,7 +119,7 @@ class Assignment_Three_Scene extends Scene_Component
         const box_size = 8;
         const row_length = 6;
         const path_length = 100;
-        const blue = Color.of( 0,0,1,1 ), yellow = Color.of( 1,1,0,1 );
+
         let model_transform = Mat4.identity().times(Mat4.scale([box_size, box_size, box_size]));
         const tr_right = 2; 
         
@@ -183,7 +138,7 @@ class Assignment_Three_Scene extends Scene_Component
           chaser_transform = chaser_transform.times(Mat4.rotation(8*t, Vec.of(1,0,0) ));
           chaser_transform = chaser_transform.times(Mat4.scale([4,4,4]));
 
-          this.shapes.sphere2.draw(graphics_state, chaser_transform, this.materials.sun);
+          this.shapes.sphere2.draw(graphics_state, chaser_transform, this.materials.path);
 
 
        //draw player
@@ -231,15 +186,14 @@ class Assignment_Three_Scene extends Scene_Component
           player_transform = player_transform.times(Mat4.translation([0,0,player_speed]));
           player_transform = player_transform.times(Mat4.scale([1,3,1]));
 
-          this.shapes.box.draw(graphics_state, player_transform, this.materials.planet_4);
+          this.shapes.box.draw(graphics_state, player_transform, this.materials.player);
 
+        // ***Camera Calculations***
         //camera position
           this.planet_1 = camera_model_transform;
 
-
         //Attach camera
-
-          var desired = Mat4.inverse(this.planet_1.times(Mat4.translation([0,0,5])));
+          let desired = Mat4.inverse(this.planet_1.times(Mat4.translation([0,0,5])));
           desired = desired.times(Mat4.scale([1,1,-1]));
           desired = desired.times(Mat4.translation([0,-10,4]));
           desired = desired.times(Mat4.translation([0,0,-1*chaser_speed]));
@@ -248,58 +202,3 @@ class Assignment_Three_Scene extends Scene_Component
 
       }
   }
-
-
-// Extra credit begins here (See TODO comments below):
-
-window.Ring_Shader = window.classes.Ring_Shader =
-class Ring_Shader extends Shader              // Subclasses of Shader each store and manage a complete GPU program.
-{ material() { return { shader: this } }      // Materials here are minimal, without any settings.
-  map_attribute_name_to_buffer_name( name )       // The shader will pull single entries out of the vertex arrays, by their data fields'
-    {                                             // names.  Map those names onto the arrays we'll pull them from.  This determines
-                                                  // which kinds of Shapes this Shader is compatible with.  Thanks to this function, 
-                                                  // Vertex buffers in the GPU can get their pointers matched up with pointers to 
-                                                  // attribute names in the GPU.  Shapes and Shaders can still be compatible even
-                                                  // if some vertex data feilds are unused. 
-      return { object_space_pos: "positions" }[ name ];      // Use a simple lookup table.
-    }
-    // Define how to synchronize our JavaScript's variables to the GPU's:
-  update_GPU( g_state, model_transform, material, gpu = this.g_addrs, gl = this.gl )
-      { const proj_camera = g_state.projection_transform.times( g_state.camera_transform );
-                                                                                        // Send our matrices to the shader programs:
-        gl.uniformMatrix4fv( gpu.model_transform_loc,             false, Mat.flatten_2D_to_1D( model_transform.transposed() ) );
-        gl.uniformMatrix4fv( gpu.projection_camera_transform_loc, false, Mat.flatten_2D_to_1D(     proj_camera.transposed() ) );
-      }
-  shared_glsl_code()            // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-    { return `precision mediump float;
-              varying vec4 position;
-              varying vec4 center;
-      `;
-    }
-  vertex_glsl_code()           // ********* VERTEX SHADER *********
-    { return `
-        attribute vec3 object_space_pos;
-        uniform mat4 model_transform;
-        uniform mat4 projection_camera_transform;
-
-        void main()
-        { 
-        }`;           // TODO:  Complete the main function of the vertex shader (Extra Credit Part II).
-    }
-  fragment_glsl_code()           // ********* FRAGMENT SHADER *********
-    { return `
-        void main()
-        { 
-        }`;           // TODO:  Complete the main function of the fragment shader (Extra Credit Part II).
-    }
-}
-
-window.Grid_Sphere = window.classes.Grid_Sphere =
-class Grid_Sphere extends Shape           // With lattitude / longitude divisions; this means singularities are at 
-  { constructor( rows, columns, texture_range )             // the mesh's top and bottom.  Subdivision_Sphere is a better alternative.
-      { super( "positions", "normals", "texture_coords" );
-        
-
-                      // TODO:  Complete the specification of a sphere with lattitude and longitude lines
-                      //        (Extra Credit Part III)
-      } }
